2e0ef288543e80d1e023189f8dcc0fab0a76879e
shehab tarek <58656735+shehab93140@users.noreply.github.com>
2024-04-19 15:33:18 +0200

    Time: 159 ms (86.01%), Space: 105 MB (29.71%) - LeetHub

---
+++ 0895-maximum-frequency-stack/0895-maximum-frequency-stack.cpp
@@ -0,0 +1,45 @@
+		
+class FreqStack {
+public:
+  
+    map<int,int> freqMap;
+
+    map<int,stack<int> > setMap;
+    
+ 
+    int maxfreq = 0;
+    
+    FreqStack() {
+        
+    }
+    
+    // insert
+    void push(int val) {
+        // frequency of x
+        int freq = freqMap[val] + 1;
+        
+
+        freqMap[val] = freq;
+        
+
+        if(freq > maxfreq)
+            maxfreq = freq;
+        
+     
+        setMap[freq].push(val);
+    }
+    
+
+    int pop() {
+
+        int top = setMap[maxfreq].top();
+        setMap[maxfreq].pop();
+        
+    
+        --freqMap[top];
+     
+        if(setMap[maxfreq].size() == 0)
+            --maxfreq;
+        return top;
+    }
+};
